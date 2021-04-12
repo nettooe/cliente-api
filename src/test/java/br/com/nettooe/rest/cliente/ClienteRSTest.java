727faa0e4207d21b3d78371965407b1fd4d8a164
Oliver Netto <netto.oe@gmail.com>
2021-04-12 02:38:43 -0300

    implementação com Quarkus

---
+++ src/test/java/br/com/nettooe/rest/cliente/ClienteRSTest.java
@@ -0,0 +1,32 @@
+package br.com.nettooe.rest.cliente;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class ClienteRSTest {
+
+//	@InjectMock
+//	private ClienteRepository repository;
+//
+//	@BeforeEach
+//	public void setup() {
+//		Cliente cliente = new Cliente();
+//		Mockito.when(repository.findByIdOptional(Mockito.anyLong())).thenReturn(Optional.of(cliente));
+////        QuarkusMock.installMockForType(mock, MockableBean1.class);  
+//	}
+//
+//	@Test
+//	public void test_getClienteById() {
+//		given().when().get("/cliente/{id}", 1).then().statusCode(200);
+//	}
+
+//	@Test
+//	@org.junit.jupiter.api.Order(1)
+//	public void test_save() {
+//		given().body(
+//				"{\"email\": \"teste@test.com\", \"dataNascimento\": \"1987-01-20\", \"nome\": \"Leonardo DaVinci\"}")
+//				.header("Content-Type", MediaType.APPLICATION_JSON).header("accept", "*/*").when().post("/cliente")
+//				.then().statusCode(201);
+//	}
+
+}
